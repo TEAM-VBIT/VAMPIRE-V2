e57220fb5a356c8e7c99f3456df43956bd11d444
ㅤ‑ один -//-【💸】 <mralone3536@gmail.com>
2026-04-16 20:25:36 +0530

    𝐂ᴏᴍᴍᴀɴᴅ 𝐅ɪx 𝐁ʏ 𝐀ʟᴏɴᴇ 𝐂ᴏᴅᴇʀ

--- config.py
+++ config.py
@@ -22,8 +22,8 @@ class Config:
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AloneUpdates")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AloneBotSupport")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/LunaBots1")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+n0Uo8KncgHxmNDU1")
 
         self.AUTO_END: bool = getenv("AUTO_END", False)
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", False)
