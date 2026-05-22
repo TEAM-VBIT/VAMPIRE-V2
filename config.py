f92cbf54ae575229d0a06fe30645195719518528
Drifter_X_Jack <vipin275869@gmail.com>
2026-05-22 13:34:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,6 +32,7 @@ class Config:
             url for url in getenv("COOKIES_URL", "").split(" ")
             if url and "batbin.me" in url
         ]
+        self.YOUTUBE_API_KEY = getenv("YOUTUBE_API_KEY", "INFLEX25174928D")
         self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://te.legra.ph/file/3e40a408286d4eda24191.jpg")
         self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/haagg2.png")
         self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/zvziwk.jpg")
