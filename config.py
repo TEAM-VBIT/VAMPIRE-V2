0d945bebf0ca139fbee5ed6cf553093a24040710
Drifter_X_Jack <vipin275869@gmail.com>
2026-05-23 23:42:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ class Config:
         self.SESSION3 = getenv("SESSION3", None)
 
         self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/LunaBots1")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+1L0u7kV8LLI0NTI1")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Luna_Support_Team")
 
         self.AUTO_END: bool = getenv("AUTO_END", False)
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", False)
