8265d3eb8c6ee09fb1194d62bf0a73c6b63635a6
VAMPIRE-BOTS <mdhakeeb11@gmail.com>
2026-05-29 15:25:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,8 +22,8 @@ class Config:
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/LunaBots1")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Luna_Support_Team")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/VAMPIREUPDATES")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/VAMPIREUPDATES")
 
         self.AUTO_END: bool = getenv("AUTO_END", False)
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", False)
@@ -34,8 +34,8 @@ class Config:
         ]
         self.YOUTUBE_API_KEY = getenv("YOUTUBE_API_KEY", "INFLEX25174928D")
         self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://te.legra.ph/file/3e40a408286d4eda24191.jpg")
-        self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/haagg2.png")
-        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/3gtxpg.jpg")
+        self.PING_IMG = getenv("PING_IMG", "https://i.ibb.co/fzQJvwB9/x.jpg")
+        self.START_IMG = getenv("START_IMG", "https://i.ibb.co/fzQJvwB9/x.jpg")
 
     def check(self):
         missing = [
