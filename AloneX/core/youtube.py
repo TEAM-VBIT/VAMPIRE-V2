589345e0c1c73d0246faafbea25ab43751bd62d0
ᯓ꯭𓆰꯭×꯭🔥⁠⁠⁠𝐕꯭𝐀꯭𝐌꯭𝐏꯭𝐈꯭𝐑꯭𝐄꯭ 𝐊꯭𝐈꯭𝐍꯭𝐆 ꯭🌾꯭ <mdkhateeb07302@gmail.com>
2026-06-01 15:45:22 +0530

    Update youtube.py

--- AloneX/core/youtube.py
+++ AloneX/core/youtube.py
@@ -13,7 +13,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
-from ytSearch import VideosSearch, Playlist
+from youtubesearchpython.__future__ import VideosSearch
+from youtubesearchpython import Playlist
 from AloneX import LOGGER
 from AloneX.utils.database import is_on_off
 from AloneX.utils.formatters import time_to_seconds
